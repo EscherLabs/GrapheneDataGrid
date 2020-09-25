1b0999064b41c1340f754359df01981181ae6f3b
nwallin1 <32525511+nwallin1@users.noreply.github.com>
2020-09-25 16:27:22 -0400

    Removing Duplicate Website Code
    Deleting _site folder in the root directory of Master. The _site folder exists within docs/ already.

--- _site/make.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# hulk ./src/views/*.mustache   > ./src/js/views.js
-minify ./src/js/GrapheneDataGrid.js ./src/js/mixin.js ./src/js/model.js ./src/js/views.js > ./bin/js/GrapheneDataGrid.min.js
-cat ./src/js/GrapheneDataGrid.js ./src/js/mixin.js ./src/js/model.js ./src/js/views.js > ./bin/js/GrapheneDataGrid.full.js
-cp ./bin/js/GrapheneDataGrid.full.js ./docs/assets/js/
